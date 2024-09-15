612e6cb1fc2da5f222747c1c8ddb16a63a82ee9e
Tahaabdul <rhaxaqi@gmail.com>
2024-09-15 23:27:00 +0000

    Task 2. Calculate the Area of a Rectangle

---
+++ python_introduction/rectangle_area.py
@@ -0,0 +1,9 @@
+# Declare variables
+length = 1000  # Length of rectangle
+width = 0.05       # Width of rectangle
+
+#Calculate Area
+area = length * width
+
+# Print the calculated interest
+print(f"The area of the rectangle is: {area}")
