d3ca05b2e43faa12ec9831c3ce90f68726de7241
Tahaabdul <rhaxaqi@gmail.com>
2024-09-15 23:16:30 +0000

    Task 0. Basic Arithmetic Exercise

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,14 @@
+# Declaring number1 & number2
+
+number1 = 10
+number2 = 5
+
+#setting the operations
+sum = number1 + number2
+sub = number1 - number2
+mul = number2 * number1
+
+# Print results in a human-readable format
+print(f"Addition of {number1} and {number2} is {sum}")
+print(f"Subtraction of {number1} and {number2} is {sub}")
+print(f"Multiplication of {number1} and {number2} is {mul}")
