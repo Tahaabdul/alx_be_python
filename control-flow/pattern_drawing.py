e5ff9916d1096ea7e13b2159ef1c51270b9509c1
Tahaabdul <rhaxaqi@gmail.com>
2024-09-22 21:58:15 +0000

    Corrected Task 3. Drawing Patterns with Nested Loops

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -1,10 +1,13 @@
 # Ask the user for the size of the pattern
 size = int(input("Enter the size of the pattern: "))
+row = size
 
 # Check if the input is positive
 if size > 0:
     # Draw the pattern
-    for row in range(size):
+    while(row > 0):
+        row -=1
         print("*" * size)  # Print a row of asterisks
+    
 else:
     print("Please enter a positive integer.")
